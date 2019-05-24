a246675677a19d1a5500d9e4957874d9c0298a21
Tran Le Thai Son <tranlethaison@gmail.com>
2019-05-24 12:13:43 +0900

    Fibonacci spiral, boys I got it

--- calculus/fibonacci_spiral.py
+++ calculus/fibonacci_spiral.py
@@ -1,64 +1,89 @@
 import numpy as np
-import cv2 as cv
-
 import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 
 from utils import create_unique_color_uchar
 
+# Plotting prepare
+fig = plt.figure()
+axe = fig.add_subplot(111)
 
-# Directions matrix for calculating new top-left
-# [[['k-1' related], ['k' related]], ...]
+# Directions matrix for calculating bottom-left of k-th square
+# wrt [[M[k-1], M[k-1]], [M[k], M[k]]]
 D = np.array([
-    [[0, 0], [-1, 0]], # left
-    [[0, 1], [ 0, 0]], # down
-    [[1, 0], [ 0, 0]], # right
-    [[0, 0], [ 0,-1]]  # up
+    [[0, 0], [-1,  0]],  # left
+    [[0, 0], [ 0, -1]],  # down
+    [[1, 0], [ 0,  0]],  # right
+    [[0, 1], [ 0,  0]]   # up
 ])
 
-# Calculate Fibonacci sequence
-n = 12
+# 1/4 of the circle for k-th square
+f = lambda x, r: (r**2 - x**2)**0.5
+# Directions matrix for calculating centroid
+D_centroid = np.array([
+    [1, 0], # left
+    [1, 1], # down
+    [0, 1], # right
+    [0, 0], # up
+])
+# Directions matrix of X range wrt [r, r]
+# ignore point [0, 0] for the sake of simple
+D_X = np.array([
+    [-1, 0], # left  => top-left 
+    [-1, 0], # down  => bottom-left
+    [ 0, 1], # right => bottom-right
+    [ 0, 1], # up    => top-right
+])
+# Directions matrix of Y range wrt to Y range
+D_Y = np.array([
+     1, # left  => top-left 
+    -1, # down  => bottom-left
+    -1, # right => bottom-right
+     1, # up    => top-right
+])
+
+# Init Fibonacci sequence
+n = 44
 M = [None] * (n + 1)
 M[0], M[1] = 0, 1
+
+# 1st Fibonacci
+k = 1
+bl = bl_prev = (0, 0)
+color = np.array(create_unique_color_uchar(k)) / 255
+axe.add_patch(patches.Rectangle(bl, width=M[k], height=M[k], fill=False, color=color))
+
+# k-th Fibonacci
 for k in range(2, n + 1):
     M[k] = M[k-1] + M[k-2]
+    direction = k % 4
 
-# Background
-size = np.array((M[n] + M[n-1], M[n] + M[n-1]))
-start = size // 2
-bg = np.full((size[1],size[0],3), 255, np.uint8)
-
-# Style
-fontFace = cv.FONT_HERSHEY_COMPLEX_SMALL
-fontScale = 1
-thickness = 1
-
-# 1st rect
-k = 1
-tl = start
-tl_prev = tl
-br = tl + M[k]
-
-color = create_unique_color_uchar(k)
-cv.rectangle(bg, tuple(tl), tuple(br), color, thickness)
-#cv.putText(bg, str(k), (tl[0],br[1]), fontFace, fontScale, color)
-
-# Other rects
-for k in range(2, n + 1):
-    tl = (
-        tl_prev +
-        [M[k-1], M[k-1]] * D[k%4][0] +
-        [M[k], M[k]] * D[k%4][1]
+    # square's bottom-left
+    bl = (
+        bl_prev +
+        D[direction][0] * [M[k-1], M[k-1]] +
+        D[direction][1] * [M[k], M[k]]
     )
-    tl_prev = tl_prev if sum(tl_prev) < sum(tl) else tl
-    br = tl + M[k]
+    # Last square's bottom-left
+    bl_prev = np.min([bl_prev, bl], axis=0)
 
-    color = create_unique_color_uchar(k)
-    cv.rectangle(bg, tuple(tl), tuple(br), color, thickness)
-    #cv.putText(bg, str(k), (tl[0],br[1]), fontFace, fontScale, color)
+    # 1/4 circle
+    centroid = bl + D_centroid[direction] * [M[k], M[k]]
+    low, hight = [M[k], M[k]] * D_X[direction]
+    X = np.linspace(low, hight, 100)
+    Y = f(X, M[k]) * D_Y[direction]
 
+    # Plot
+    color = np.array(create_unique_color_uchar(k)) / 255
+    axe.add_patch(patches.Rectangle(bl, width=M[k], height=M[k], fill=False, color=color))
+    axe.plot(X + centroid[0], Y + centroid[1], color=color)
 
-plt.figure()
-plt.imshow(bg)
-plt.axis(False)
+    print('{:2d}. {} / {} = {}'.format(k, M[k], M[k-1], M[k] / M[k-1]))
+print('Golden ratio: {}'.format((1 + 5**0.5) / 2))
+
+# Show
+# lims = np.array([-M[k] - M[k-1], M[k]])
+# plt.xlim(lims)
+# plt.ylim(-lims[::-1])
+axe.set_aspect('equal')
 plt.show()
-
--- test/simple_pg.sh
+++
@@ -1,2 +0,0 @@
-python learnRl/simple_pg.py train --env-name 'CartPole-v0' --lr 0.05
-
