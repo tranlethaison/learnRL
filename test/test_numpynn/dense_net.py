2bf08de522147f7f266abb63591538a3e31735c1
starship <starship@example.com>
2019-09-19 17:19:26 +0900

    minor changes

--- test/test_numpynn/dense_net.py
+++ test/test_numpynn/dense_net.py
@@ -2,7 +2,7 @@ import sys
 sys.path.append(".")
 
 import numpy as np
-from keras.datasets import mnist
+from keras.datasets import mnist, fashion_mnist
 
 from numpynn.layers import Dense
 from numpynn.activations import Sigmoid
@@ -15,11 +15,17 @@ from numpynn.losses import MSE
 if __name__ == "__main__":
     # << Data
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    # (x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
+
+    n = 50000 
+    x_train, x_val = x_train[:n], x_train[n:]
+    y_train, y_val = y_train[:n], y_train[n:]
 
     def preprocess(x):
         return np.expand_dims((x / 255.0).reshape(x.shape[0], -1), axis=-1)
 
     x_train = preprocess(x_train)
+    x_val = preprocess(x_val)
     x_test = preprocess(x_test)
 
     eye = np.eye(10)
@@ -33,4 +39,7 @@ if __name__ == "__main__":
 
     model = Model(inputs=inputs, outputs=outputs)
     model.compile(optimizer=SGD(lr=3.0), loss=MSE)
-    model.fit(x_train, y_train, batch_size=10, epochs=30, test_data=(x_test, y_test))
+
+    model.fit(x_train, y_train, batch_size=10, epochs=30, test_data=(x_val, y_val))
+    accuracy = model.evaluate(x_test, y_test) / len(x_test)
+    print("Accuracy:", accuracy)
